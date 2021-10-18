fa1cb5b8b7b0a795d2d580985b13702e583747bb
Victor Fulgencio <victor.andrade4@hotmail.com>
2021-10-18 01:01:22 -0300

    add homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  currentYear = new Date().getFullYear();
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
